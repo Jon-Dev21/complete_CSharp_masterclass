9dc7d8358e5d38573b6dd07230ca245845e10304
Jon-Dev21 <jonacevedev96@gmail.com>
2021-10-25 16:38:22 -0400

    Added new solution Tic-tac-toe. In this solution, I'll be attempting to create a console based version of the famous game tic-tac-toe using the knowledge ackquired during this C# Course.

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    /// <summary>
+    /// This program is a console based Tic-Tac-Toe game.
+    /// Created By. Jonathan A. Acevedo
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
+            ClearConsole();
+            
+            Console.WriteLine("Tic-Tac-Toe");
+            Console.WriteLine("By Jonathan A. Acevedo");
+
+            Console.WriteLine();
+            ShowGrid();
+        }
+
+        /// <summary>
+        /// This function prints out the tic-tac-toe game board
+        /// </summary>
+        public static void ShowGrid()
+        {
+            for(int i = 0; i<grid.GetLength(0); i++)
+            {
+                Console.WriteLine("     |     |      ");
+                
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    Console.Write(j!=2?"  "+ grid[i,j]+"  |": "  " + grid[i, j] + "   ");
+                }
+                Console.WriteLine();
+                Console.WriteLine(i!=2?"_____|_____|_____ ": "     |     |      ");
+
+            }
+        }
+        /// <summary>
+        /// This function clears the console when called.
+        /// </summary>
+        public static void ClearConsole()
+        {
+            Console.Clear();
+        }
+ 
+
+
+
+        /// <summary>
+        /// This two-dimensional string array represents the tic tac toe grid.
+        /// </summary>
+        static string[,] grid = new string[3, 3] { 
+            {"1" , "2", "3"},
+            {"4" , "5", "6"},
+            {"7" , "8", "9"}
+        };
+    }
+}
