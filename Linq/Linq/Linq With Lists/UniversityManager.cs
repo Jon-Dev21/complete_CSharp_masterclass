aa1df745e1013bc838cae12d754bb38645d4662f
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-03 12:13:08 -0400

    Learned about sorting and filtering data with Linq.
    Added more methods to the UniversityManager class to demonstrate sorting, filtering
    and searching for specific data using Linq.
    
    Learned how to create collections based on other collections using Linq.
    
    Learned how to use Linq with XML.
    
    Learned how to reverse arrays of integers using the reverse method and using the descending keyword in the query.
    
    Now, will be creating new project in WPF "LinqToSQL" to finish course section.

--- Linq/Linq/Linq With Lists/UniversityManager.cs
+++ Linq/Linq/Linq With Lists/UniversityManager.cs
@@ -31,10 +31,13 @@ namespace Linq.Linq_With_Lists
             students.Add(new Student { Id = 6, Name = "Becky", Gender = "Female", Age = 17, UniversityId = 2 });
         }
 
-        // Will give us all the male students.
+        /// <summary>
+        /// Will give us all the male students.
+        /// </summary>
         public void MaleStudents()
         {
             IEnumerable<Student> maleStudent = from student in students where student.Gender == "Male" select student;
+            Console.WriteLine("=================== Male Students ===================");
             foreach (Student student in maleStudent)
             {
                 student.Print();
@@ -42,10 +45,13 @@ namespace Linq.Linq_With_Lists
             }
         }
 
-        // Will give us all the female students
+        /// <summary>
+        /// Will give us all the female students
+        /// </summary>
         public void FemaleStudents()
         {
             IEnumerable<Student> femaleStudent = from student in students where student.Gender == "Female" select student;
+            Console.WriteLine("=================== Female Students ===================");
             foreach (Student student in femaleStudent)
             {
                 student.Print();
@@ -53,15 +59,117 @@ namespace Linq.Linq_With_Lists
             }
         }
 
-        // Will give us all the transgender students
+        /// <summary>
+        /// Will give us all the transgender students
+        /// </summary>
         public void TransgenderStudents()
         {
             IEnumerable<Student> transgenderStudent = from student in students where student.Gender == "Transgender" select student;
+            Console.WriteLine("=================== Transgender Students ===================");
             foreach (Student student in transgenderStudent)
             {
                 student.Print();
                 Console.WriteLine();
             }
         }
+
+        /// <summary>
+        /// Create a list of students sorted by age.
+        /// </summary>
+        public void SortStudentByAge()
+        {
+            var sortedStudents = from student in students orderby student.Age select student;
+            Console.WriteLine("=================== Students by Age ===================");
+            foreach (Student student in sortedStudents)
+            {
+                student.Print();
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Prints a list of all students that attend Beijing Tech
+        /// </summary>
+        public void AllStudentsFromBeijingTech()
+        {
+            // List of all students from Beijing Tech
+            IEnumerable<Student> beijingTechStudents = from student in students
+                                                       join university in universities on student.UniversityId equals university.Id
+                                                       where university.Name == "Beijing Tech"
+                                                       select student;
+            Console.WriteLine("=================== Students From Beijing Tech ===================");
+            foreach (Student student in beijingTechStudents)
+            {
+                student.Print();
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Prints a list of all students that attend Beijing Tech
+        /// </summary>
+        public void AllStudentsFromYale()
+        {
+            // List of all students from Beijing Tech
+            IEnumerable<Student> yaleStudents = from student in students
+                                                       join university in universities on student.UniversityId equals university.Id
+                                                       where university.Name == "Yale"
+                                                       select student;
+            Console.WriteLine("=================== Students From Yale ===================");
+            foreach (Student student in yaleStudents)
+            {
+                student.Print();
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Returns students from the specific university Id passed.
+        /// </summary>
+        /// <param name="id"></param>
+        public void AllStudentsFromSpecifiedUniversityId(int id)
+        {
+            IEnumerable<Student> studentsFromSpecificUni = from student in students
+                                                           join university in universities on student.UniversityId equals university.Id
+                                                           where university.Id == id
+                                                           select student;
+
+            
+            if (id == 1 || id == 2)
+            {
+                Console.WriteLine("=================== Students From {0} ===================", id == 1 ? "Yale": "Beijing Tech");
+                foreach (Student student in studentsFromSpecificUni)
+                {
+                    student.Print();
+                    Console.WriteLine();
+                }
+            } else
+            {
+                Console.WriteLine("University not found...");
+            }
+            
+
+        }
+
+        /// <summary>
+        /// Prints a collection of all students and their universities.
+        /// </summary>
+        public void StudentAndUniversityNameCollection()
+        {
+            // We fetch all students and their university, 
+            // order them by their name and put their names
+            // in a new object together with the university name
+            var newCollection = from student in students
+                                join university in universities on student.UniversityId equals university.Id
+                                orderby student.Name
+                                select new { StudentName = student.Name, UniversityName = university.Name };
+
+            Console.WriteLine("Students and their universities: \n");
+            foreach (var collection in newCollection)
+            {
+                Console.WriteLine("Student: {0}\nUniversity: {1}\n", collection.StudentName, collection.UniversityName);
+            }
+        }
+
     }
 }
--- Linq/Linq/Program.cs
+++ Linq/Linq/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace Linq
 {
@@ -14,9 +15,9 @@ namespace Linq
             // ============================================
 
             // Created array of numbers.
-            //int[] numbers = {1,2,3,4,5,6,7,8,9 };
-            //// This method uses Linq to assign odd numbers to an IEnumerable List of integers and print it.
-            //OddNumbers(numbers);
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // This method uses Linq to assign odd numbers to an IEnumerable List of integers and print it.
+            OddNumbers(numbers);
 
             // ============================================
             // ============= Linq With Lists ==============
@@ -27,12 +28,132 @@ namespace Linq
             // Creating new class "UniversityManager"
 
             UniversityManager universityManager = new UniversityManager();
-            Console.WriteLine("=================== Male Students ===================");
+
             universityManager.MaleStudents();
-            Console.WriteLine("=================== Female Students ===================");
+
             universityManager.FemaleStudents();
-            Console.WriteLine("=================== Transgender Students ===================");
+
             universityManager.TransgenderStudents();
+
+            // ============================================
+            // ====== Sorting & filtering with Linq =======
+            // ============================================
+
+            universityManager.SortStudentByAge();
+
+            universityManager.AllStudentsFromBeijingTech();
+
+            universityManager.AllStudentsFromYale();
+
+            Console.Write("Enter university Id: ");
+            string id = Console.ReadLine();
+            int numId = Convert.ToInt32(id);
+            universityManager.AllStudentsFromSpecifiedUniversityId(numId);
+
+
+            // How to reverse sorted data.
+            int[] someInts = { 12, 453, 6236, 2345, 1, 237, 245, 1, 345, 134, 6, 45, 72, 4 };
+            IEnumerable<int> sortedInts = from i in someInts orderby i select i;
+            IEnumerable<int> reversedInts = sortedInts.Reverse();
+
+            Console.Write("Sorted ints: ");
+
+            foreach (int num in sortedInts)
+            {
+                Console.Write(num + ", ");
+            }
+            Console.WriteLine();
+            Console.Write("Reverse Sorted ints: ");
+
+            foreach (int num in reversedInts)
+            {
+                Console.Write(num + ", ");
+            }
+
+            // Alternate way to reverse an array of integers.
+            IEnumerable<int> otherSortedInts = from i in someInts orderby i descending select i;
+
+            Console.Write("\n\nAlternate way to reverse array of integers\nReverse Sorted ints: ");
+
+            foreach (int num in reversedInts)
+            {
+                Console.Write(num + ", ");
+            }
+
+            // ============================================
+            // ====== Creating collections based ==========
+            // ======    on other Collections    ==========
+            // ============================================
+
+            universityManager.StudentAndUniversityNameCollection();
+
+            // ============================================
+            // =============== Linq and XML ===============
+            // ============================================
+
+            string studentsXML = @"<Students>
+                                     <Student>
+                                         <Name>Toni</Name>
+                                         <Age>21</Age>
+                                         <University>Yale</University>
+                                         <Semester>1</Semester>
+                                     </Student>
+                                     <Student>
+                                         <Name>Carla</Name>
+                                         <Age>17</Age>
+                                         <University>Yale</University>
+                                         <Semester>2</Semester>
+                                     </Student>
+                                     <Student>
+                                         <Name>Leyla</Name>
+                                         <Age>19</Age>
+                                         <University>Beijing Tech</University>
+                                         <Semester>3</Semester>
+                                     </Student>
+                                     <Student>
+                                         <Name>Kenan</Name>
+                                         <Age>23</Age>
+                                         <University>Beijing Tech</University>
+                                         <Semester>4</Semester>
+                                     </Student>
+                                   </Students>";
+
+            // using System.Xml.Linq;
+            XDocument studentsXdoc = new XDocument();
+            studentsXdoc = XDocument.Parse(studentsXML);
+
+            var students = from student in studentsXdoc.Descendants("Student")
+                           select new
+                           {
+                               Name = student.Element("Name").Value,
+                               Age = student.Element("Age").Value,
+                               University = student.Element("University").Value,
+                               Semester = student.Element("Semester").Value
+                           };
+            Console.WriteLine("Printing students from XML: \n");
+            foreach (var student in students)
+            {
+                Console.WriteLine("Student: {0}\nAge: {1}\nUniversity: {2}\nSemester: {3}\n", student.Name, student.Age, student.University, student.Semester);
+            }
+
+            // Challenge, sort students by their age
+            var studentsByAge = from student in students
+                                orderby student.Age
+                                select student;
+            Console.WriteLine("\nPrinting students from XML Ordered by age: \n");
+            foreach (var student in studentsByAge)
+            {
+                Console.WriteLine("Student: {0}\nAge: {1}\nUniversity: {2}\nSemester: {3}\n", student.Name, student.Age, student.University, student.Semester);
+            }
+
+            // Challenge: Add another type of information and another student in the XML.
+            // Added <Semester> tag to students
+
+
+            // ============================================
+            // ==== Setting up Project for LinqToSQL  =====
+            // ============================================
+            // Will be creating new WPF Project, LinqToSQL.
         }
 
         /// <summary>
