8424435be15f6133f412594ab084c80970ac6d03
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-03 17:08:02 -0400

    Added new "Threads" solution. This solution will cover the first half of the Threads section in this course.
    For now, covered Threads basics.
    
    Learned how to use threads.
    Learned how to create new threads (but not name them).

---
+++ Threads/Threads/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ==========================================
+            // ============= Threads Basics =============
+            // ==========================================
+
+            // This only pauses the main thread.
+
+            // Pause program for 1 second
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World! 1");
+            //// Pause program for 1 second
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World! 2");
+            //// Pause program for 1 second
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World! 3");
+            //// Pause program for 1 second
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World! 4");
+
+            // Creating a new threads
+            new Thread(() => {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1.");
+            }).Start();
+
+            new Thread(() => {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2.");
+            }).Start();
+
+            new Thread(() => {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3.");
+            }).Start();
+
+            new Thread(() => {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4.");
+            }).Start();
+
+        }
+    }
+}
